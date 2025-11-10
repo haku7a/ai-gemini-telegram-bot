c0c769b4c8fb3270413e9ae6ebfa4cd769523d8d
haku7a <v.a.neyaskin@gmail.com>
2025-11-10 05:59:53 +0300

    feat: add initial /start command handler
    - Initialize the handlers module with its own router.
    - Connect the user commands router to the main dispatcher.

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from os import getenv
 from aiogram import Bot, Dispatcher
 
 from config import TOKEN
+from handlers import user_commands
 
 dp = Dispatcher()
 
 
 async def main() -> None:
+    dp.include_router(user_commands.router)
+
     bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
