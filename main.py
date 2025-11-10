94ad94f97883b9536c5f6af4eb7b73b26774fc36
haku7a <v.a.neyaskin@gmail.com>
2025-11-10 05:26:15 +0300

    feat: initial bot setup

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from os import getenv
+
+from aiogram import Bot, Dispatcher
+
+TOKEN = getenv("BOT_TOKEN")
+dp = Dispatcher()
+
+
+async def main() -> None:
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
